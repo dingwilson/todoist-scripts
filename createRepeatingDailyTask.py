8d28cde74a418913d52ea5ea8c8b26125af9dc83
Wilson Ding <hello@wilsonding.com>
2019-12-14 11:28:57 -0800

    updated to v1 api

--- createRepeatingDailyTask.py
+++ createRepeatingDailyTask.py
@@ -8,8 +8,8 @@ token = file.readline()
 ### SETTINGS:
 
 content = "Run 1 Mile" # Task Name
-start_date = date(2018, 11, 1) # Select a start date -> format: date(YYYY, M, D)
-end_date = date(2019, 1, 1) # Select an end date (non-inclusive) -> format: date(YYYY, M, D)
+start_date = date(2020, 1, 1) # Select a start date -> format: date(YYYY, M, D)
+end_date = date(2021, 1, 1) # Select an end date (non-inclusive) -> format: date(YYYY, M, D)
 
 ###
 
@@ -18,13 +18,15 @@ def daterange(start_date, end_date):
         yield start_date + timedelta(n)
 
 for due_date in daterange(start_date, end_date):
-	r = requests.post("https://beta.todoist.com/API/v8/tasks",
-	    params={"token": token},
-	    data=json.dumps({"content": content,
-	                     "due_date": due_date.strftime("%Y-%m-%d"),
-	                     }),
-	    headers={
-	        "Content-Type": "application/json",
-	        "X-Request-Id": str(uuid.uuid4()),
-	    }
-	)
+	requests.post("https://api.todoist.com/rest/v1/tasks",
+    data=json.dumps({
+        "content": content,
+        "due_string": due_date.strftime("%Y-%m-%d"),
+        "due_lang": "en",
+        "priority": 4
+    }),
+    headers={
+        "Content-Type": "application/json",
+        "X-Request-Id": str(uuid.uuid4()),
+        "Authorization": "Bearer %s" % token
+    }).json()
